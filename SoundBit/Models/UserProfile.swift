bff55c0b3c986abc5b11e969af42777fda2ce4f1
davalcato <davalcato@yahoo.com>
2021-02-23 21:42:40 -0800

    Created struct UserProfile and added let country: String

--- SoundBit/Models/UserProfile.swift
+++ SoundBit/Models/UserProfile.swift
@@ -8,10 +8,40 @@
 import Foundation
 
 struct UserProfile {
-    
+    let country: String
+    let display_name: String
+    let email: String
     
 }
 
+//{
+//    country = US;
+//    "display_name" = "Lindsey Leeson";
+//    email = "bigpaper4me@yahoo.com";
+//    "explicit_content" =     {
+//        "filter_enabled" = 0;
+//        "filter_locked" = 0;
+//    };
+//    "external_urls" =     {
+//        spotify = "https://open.spotify.com/user/128342213";
+//    };
+//    followers =     {
+//        href = "<null>";
+//        total = 0;
+//    };
+//    href = "https://api.spotify.com/v1/users/128342213";
+//    id = 128342213;
+//    images =     (
+//                {
+//            height = "<null>";
+//            url = "https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-1/c37.52.233.233a/76529_168849003135156_5012213_n.jpg?_nc_cat=103&ccb=3&_nc_sid=0c64ff&_nc_ohc=vIPhmZtBHcMAX-zYmwa&_nc_ht=scontent-dfw5-1.xx&_nc_tp=29&oh=1280a1886fe3b38a75bd123e8876f865&oe=605C0C5F";
+//            width = "<null>";
+//        }
+//    );
+//    product = open;
+//    type = user;
+//    uri = "spotify:user:128342213";
+//}
 
 
 
