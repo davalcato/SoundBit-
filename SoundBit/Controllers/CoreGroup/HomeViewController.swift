348b0e1d21f8fb684ea55d66d168af949a35b45a
davalcato <davalcato@yahoo.com>
2021-03-01 22:34:10 -0800

    added the item.contentInsets = NSDirectionalEdgeInsets to the private static func createSectionLayout in the HomeViewController

--- SoundBit/Controllers/CoreGroup/HomeViewController.swift
+++ SoundBit/Controllers/CoreGroup/HomeViewController.swift
@@ -62,6 +62,12 @@ class HomeViewController: UIViewController {
             layoutSize: NSCollectionLayoutSize(
                 widthDimension: .fractionalWidth(1.0),
                 heightDimension: .fractionalHeight(1.0)))
+        
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2,
+                                                     leading: 2,
+                                                     bottom: 2,
+                                                     trailing: 2)
+        
         // Group
         let group = NSCollectionLayoutGroup.vertical(
             layoutSize: NSCollectionLayoutSize(
