f7bccc7616a17591bb194dce6458f4adb7156807
davalcato <davalcato@yahoo.com>
2021-03-02 00:09:17 -0800

    Added the enum BrowseSectionType to create the 3 different section layouts in the HomeViewController

--- SoundBit/Controllers/CoreGroup/HomeViewController.swift
+++ SoundBit/Controllers/CoreGroup/HomeViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+// Provider a different section layout here
+enum BrowseSectionType {
+    // Sections
+    case newReleases // 1
+    case featuredPlaylists // 2
+    case recommendedTracks // 3
+}
+
 class HomeViewController: UIViewController {
     
     private var collectionView: UICollectionView =  UICollectionView(
@@ -59,44 +67,146 @@ class HomeViewController: UIViewController {
     
     // Creates the layout here
     private static func createSectionLayout(section: Int) -> NSCollectionLayoutSection {
-        // Item
-        let item = NSCollectionLayoutItem(
-            layoutSize: NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1.0),
-                heightDimension: .fractionalHeight(1.0)))
-        
-        item.contentInsets = NSDirectionalEdgeInsets(top: 2,
-                                                     leading: 2,
-                                                     bottom: 2,
-                                                     trailing: 2)
-        
-        // Vertical group inside the horizontal group
-        let verticalGroup = NSCollectionLayoutGroup.vertical(
-            layoutSize: NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1.0),
-                heightDimension: .absolute(360)),
-            subitem: item,
-            count: 3
-        )
-        
-        
-        let horizontalGroup = NSCollectionLayoutGroup.horizontal(
-            layoutSize: NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1.0),
-                heightDimension: .absolute(360)),
-            subitem: verticalGroup,
-            count: 1
-        )
-        
-        // Section
-        let section = NSCollectionLayoutSection(group: horizontalGroup)
-        section.orthogonalScrollingBehavior = .continuous
-        return section
+        switch section {
+        case 0:
+            // Item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)))
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2,
+                                                         leading: 2,
+                                                         bottom: 2,
+                                                         trailing: 2)
+            
+            // Vertical group inside the horizontal group
+            let verticalGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(390)),
+                subitem: item,
+                count: 3
+            )
+            
+            
+            let horizontalGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.9),
+                    heightDimension: .absolute(390)),
+                subitem: verticalGroup,
+                count: 1
+            )
+            
+            // Section
+            let section = NSCollectionLayoutSection(group: horizontalGroup)
+            // This snapped the page views together
+            section.orthogonalScrollingBehavior = .groupPaging
+            return section
+            
+        case 1:
+            // Item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)))
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2,
+                                                         leading: 2,
+                                                         bottom: 2,
+                                                         trailing: 2)
+            
+            // Vertical group inside the horizontal group
+            let verticalGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(390)),
+                subitem: item,
+                count: 3
+            )
+            
+            
+            let horizontalGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.9),
+                    heightDimension: .absolute(390)),
+                subitem: verticalGroup,
+                count: 1
+            )
+            
+            // Section
+            let section = NSCollectionLayoutSection(group: horizontalGroup)
+            // This snapped the page views together
+            section.orthogonalScrollingBehavior = .groupPaging
+            return section
+            
+        case 2:
+            // Item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)))
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2,
+                                                         leading: 2,
+                                                         bottom: 2,
+                                                         trailing: 2)
+            
+            // Vertical group inside the horizontal group
+            let verticalGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(390)),
+                subitem: item,
+                count: 3
+            )
+            
+            
+            let horizontalGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.9),
+                    heightDimension: .absolute(390)),
+                subitem: verticalGroup,
+                count: 1
+            )
+            
+            // Section
+            let section = NSCollectionLayoutSection(group: horizontalGroup)
+            // This snapped the page views together
+            section.orthogonalScrollingBehavior = .groupPaging
+            return section
+            
+        default:
+            // Item
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)))
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2,
+                                                         leading: 2,
+                                                         bottom: 2,
+                                                         trailing: 2)
+            
+            // Vertical group inside the horizontal group
+            let group = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .absolute(390)),
+                subitem: item,
+                count: 1
+            )
+            // Section
+            let section = NSCollectionLayoutSection(group: group)
+            return section
+        }
     }
     
     // Make multiple API calls from here 
     private func fetchData() {
         // Featured Playlists
+        
+        
         // Recommended Tracks
         // New Releases
         
@@ -132,17 +242,26 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return 5
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .systemGreen
-        
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemGreen
+        }
+        else if indexPath.section == 1 {
+            cell.backgroundColor = .systemRed
+        }
+        else if indexPath.section == 2 {
+            cell.backgroundColor = .systemBlue
+        }
         return cell
     }
-    
-    
     
 }
 
