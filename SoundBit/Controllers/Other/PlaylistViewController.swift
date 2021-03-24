fd5182d874fe85b70a3f9a9e3aff25625cdfb909
davalcato <davalcato@yahoo.com>
2021-03-24 12:24:56 -0700

    Added model.tracks.items.compactMap RecommendedTrackCellViewModel to the APICaller.shared.getPlaylistDetails on the PlaylistViewController

--- SoundBit/Controllers/Other/PlaylistViewController.swift
+++ SoundBit/Controllers/Other/PlaylistViewController.swift
@@ -45,8 +45,10 @@ class PlaylistViewController: UIViewController {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError()
     }
+    // ViewModel array on the init
+    private var viewModels = [RecommendedTrackCellViewModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,12 +64,24 @@ class PlaylistViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        APICaller.shared.getPlaylistDetails(for: playlist) { result in
-            switch result {
-            case .success(let model):
-                break
-            case .failure(let error):
-                break
+        APICaller.shared.getPlaylistDetails(for: playlist) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let model):
+                    
+                    // RecommendedTrackCellViewModel
+                    self?.viewModels = model.tracks.items.compactMap({
+                        RecommendedTrackCellViewModel(
+                            name: $0.track.name,
+                            artistName: $0.track.artists.first?.name ?? "-",
+                            artworkURL: URL(string: $0.track.album?.images.first?.url ?? "")
+                        )
+                    })
+                    self?.collectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    break
+                }
             }
         }
     }
@@ -79,7 +93,7 @@ class PlaylistViewController: UIViewController {
 }
 extension PlaylistViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return viewModels.count
     }
    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -90,13 +104,15 @@ extension PlaylistViewController: UICollectionViewDelegate, UICollectionViewData
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendedTrackCollectionViewCell.identifier, for: indexPath) as? RecommendedTrackCollectionViewCell else {
             return UICollectionViewCell()
         }
-        cell.backgroundColor = .red
-//        cell.configure(with: viewModels[indexPath.row])
+    
+        cell.configure(with: viewModels[indexPath.row])
         return cell
     }
     // Tap on cell here
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+        
+        
         // Play song
         
     }
