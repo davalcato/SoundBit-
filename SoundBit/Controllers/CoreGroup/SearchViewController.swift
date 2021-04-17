f89da0dab42d84cf924fdaf4f53d191d063e1f76
davalcato <davalcato@yahoo.com>
2021-04-17 13:47:41 -0700

    Added the searchController to display the song, artist, album.

--- SoundBit/Controllers/CoreGroup/SearchViewController.swift
+++ SoundBit/Controllers/CoreGroup/SearchViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    // Top view search bar
+    let searchController: UISearchController = {
+        let results = UIViewController()
+        results.view.backgroundColor = .red
+        let vc = UISearchController(searchResultsController: results)
+        vc.searchBar.placeholder = "Songs, Artists, Albums"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        
+        return vc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        navigationItem.searchController = searchController
     }
   
 }
